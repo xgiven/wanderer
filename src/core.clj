3b60efbf1c4ea91836f4e455b536078ff81ad4a3
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-14 10:21:33 -0400

    Update core.clj

--- src/core.clj
+++ src/core.clj
@@ -13,16 +13,16 @@
   (fn [state start] ;; -> [state-new end]
     (let [
            state-tr (transient state)
-           state-new (do
-                       (loop [pos start]
-                         (let [parsed (parse-line file-vec pos)]
-                           (if (parsed :cond?) pos
-                             (do
-                               (merge! state-tr
-                                     (proc-line (nth file-vec pos)
-                                                graph-agent)))
-                             (recur (alt-fns ...))
-                         )
-                       (persistent! state-tr))
-           end TODO]
+           [state-new end] (do
+                       (let [
+                              end-found (loop [pos start]
+                                          (let [parsed (parse-line file-vec pos)]
+                                            (if (parsed :cond?) pos
+                                              (do
+                                                (merge! state-tr
+                                                        (proc-line (nth file-vec pos)
+                                                                   graph-agent)))
+                                              (recur
+                                                (or (:target parsed) (inc pos)) ))))]
+                         [(persistent! state-tr) end-found])
       [state-new end])))
