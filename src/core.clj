c664952072563411b0bdd588873b04a2cd77afba
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-14 13:08:03 -0400

    Update core.clj

--- src/core.clj
+++ src/core.clj
@@ -2,12 +2,24 @@
   (:require iota
             [instaparse.core :as insta]))
 
+(declare proc-file proc-segm parse-instr mk-linker)
+
+(def proc-file
+  "Process a file, using (#'wanderer.core/mk-linker),
+   by recursively forking when a conditional is reached,
+   and continuing in both cases independently. Note that
+   this uses agents, so you have to remember to call
+   #'clojure.core/shutdown-agents at the end of your
+   program. "
+  [filename]
+  (throw (UnsupportedOperationException. "[#'wanderer.core/proc-file] is not yet implemented")))
+
 (def proc-segm
   "Process a segment of code (up until a conditional) by
    updating the state internally (as a transient),
    updating the graph by sending linker functions to
    the agent that stores the current graph
-   (with 'wanderer.core/mk-linker), and finally, returning
+   (with #'wanderer.core/mk-linker), and finally, returning
    the new state as well as where it left off. "
   [file-vec graph-agent] ;; -> (fn-type [state start] [state-new end])
   (fn [state start] ;; -> [state-new end]
@@ -15,13 +27,13 @@
            state-tr (transient state)]
       (let [
              end-found (loop [pos start]
-                         (let [parsed (parse-instr (nth file-vec pos))] ;; extern 'wanderer.core/parse-instr
+                         (let [parsed (parse-instr (nth file-vec pos))] ;; EXTERN #'wanderer.core/parse-instr
                            (if ((comp :conditional :flow) parsed) pos ;; Only process unconditional instructions
                              (do
                                (run! (partial apply assoc! state-tr) ;; Update the current state
                                      (map :mut parsed))
                                (send graph-agent ;; Build the graph
-                                     (apply mk-linker ;; extern 'wanderer.core/mk-linker
+                                     (apply mk-linker ;; EXTERN #'wanderer.core/mk-linker
                                             ((juxt :sources :target) parsed))) ;; [sources target]
                                (recur
                                  (or (:flow parsed) (inc pos)))))))] ;; If no target exists
