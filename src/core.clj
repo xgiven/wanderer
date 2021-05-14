2cf1f414577100099c2dc8a855538b2b75c00d54
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-14 11:02:48 -0400

    Update core.clj

--- src/core.clj
+++ src/core.clj
@@ -6,22 +6,22 @@
   "Process a segment of code (up until a conditional) by
    updating the state internally (as a transient),
    updating the graph by sending linker functions to
-   the agent that stores the current graph (with mk-linker),
-   and finally, returning the new state as well as where it
-   left off. "
+   the agent that stores the current graph
+   (with 'wanderer.core/mk-linker), and finally, returning
+   the new state as well as where it left off. "
   [file-vec graph-agent] ;; -> (fn-type [state start] [state-new end])
   (fn [state start] ;; -> [state-new end]
     (let [
            state-tr (transient state)]
       (let [
              end-found (loop [pos start]
-                         (let [parsed (parse-line (nth file-vec pos)] ;; extern 'parse-line
-                           (if (parsed :flow/cond?) pos
+                         (let [parsed (parse-line (nth file-vec pos))] ;; extern 'wanderer.core/parse-line
+                           (if (parsed :flow/condition) pos
                              (do
                                (run! (partial apply assoc! state-tr)
                                      (map :mut parsed))
                                (send graph-agent
-                                     (mk-linker
+                                     (mk-linker ;; extern 'wanderer.core/mk-linker
                                        ((juxt :op/sources :op/target) parsed)))
                                (recur
                                  (or (:flow/target parsed) (inc pos)))))))]
