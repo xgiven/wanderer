952bdb1ce76c96a48dd25ef9104961cf62cc4fbd
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-14 09:43:57 -0400

    Update core.clj

--- src/core.clj
+++ src/core.clj
@@ -1,3 +1,12 @@
 (ns wanderer.core
   (:require iota
             [instaparse.core :as insta]))
+
+(def process-segm
+  [file-vec] ;; -> (fn-type [state start] [state-new end])
+  (fn [state start] ;; -> [state-new end]
+    (let [
+           tr (transient state)
+           state-new (persistent! TODO)
+           end TODO]
+      [state-new end])))
