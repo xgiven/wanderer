20164804e2a3d772961021c91231ad3a3ede3cf1
xgiven <84087902+xgiven@users.noreply.github.com>
2021-05-14 09:57:17 -0400

    Update core.clj

--- src/core.clj
+++ src/core.clj
@@ -4,9 +4,11 @@
 
 (def process-segm
   "Process a segment of code (up until a conditional) by
-   updating the state internally (as a transient) and
+   updating the state internally (as a transient),
    updating the graph by sending linker functions to
-   the agent that stores the current graph (with mk-linker)"
+   the agent that stores the current graph (with mk-linker),
+   and finally, returning the new state as well as where it
+   left off. "
   [file-vec graph-agent] ;; -> (fn-type [state start] [state-new end])
   (fn [state start] ;; -> [state-new end]
     (let [
